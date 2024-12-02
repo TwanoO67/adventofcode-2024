a06b9dde5f79407b30ef1ccec68710eb9e861680
WEBER Antoine <antoine.weber@playplay.com>
2024-12-02 22:18:07 +0100

    day2 part2

--- day2/lib.ts
+++ day2/lib.ts
@@ -4,14 +4,7 @@ export function isSafe(report: string): boolean {
     const levels = splitValues(report, ' ').map(Number);
 
     try {
-        if(levels[0] < levels[1]){
-            isIncreasing(levels);
-        }
-        else if(levels[0] >= levels[1]){
-            isDecreasing(levels);
-        }
-
-        isStepped(levels);
+        levelsAreSafe(levels)
     }
     catch(e) {
         return false;
@@ -20,10 +13,49 @@ export function isSafe(report: string): boolean {
     return true;
 }
 
+export function levelsAreSafe(levels: number[]) {
+    if(levels[0] < levels[1]){
+        isIncreasing(levels);
+    }
+    else if(levels[0] >= levels[1]){
+        isDecreasing(levels);
+    }
+
+    isStepped(levels);
+}
+
+export function isSafeWithTolerance(report: string): boolean {
+    const levels = splitValues(report, ' ').map(Number);
+
+    try {
+        levelsAreSafe(levels)
+    }
+    catch(e) {
+        const withoutOne = [...levels];
+        withoutOne.splice(e as number, 1);
+        try {
+            //try without this level
+            levelsAreSafe(withoutOne);
+        } catch {
+            try {
+                //try without next level
+                const withoutNext = [...levels]
+                withoutNext.splice((e as number) + 1, 1);
+                levelsAreSafe(withoutNext);
+            } catch {
+                //not safe
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 function isIncreasing(levels: number[]) {
     for (let i = 0; i < levels.length - 1; i++) {
         if (levels[i] > levels[i + 1]) {
-            throw i+1;
+            throw i;
         }
     }
 }
@@ -31,7 +63,7 @@ function isIncreasing(levels: number[]) {
 function isDecreasing(levels: number[]) {
     for (let i = 0; i < levels.length - 1; i++) {
         if (levels[i] < levels[i + 1]) {
-            throw i+1;
+            throw i;
         }
     }
 }
@@ -40,7 +72,7 @@ function isStepped(levels: number[]) {
     for (let i = 0; i < levels.length - 1; i++) {
         const diff = Math.abs(levels[i] - levels[i + 1]);
         if (diff === 0 || diff > 3) {
-            throw i+1;
+            throw i;
         }
     }
 }
--- day2/part-2.ts
+++ day2/part-2.ts
@@ -1,5 +1,15 @@
-import * as lib from './lib.ts';
+import { splitValues } from '../lib/lib';
+import { isSafeWithTolerance } from './lib';
 
 export const main = async (input: string) => {
-  return input;
+  const reports = splitValues(input, '\n');
+
+  const count = reports.reduce((acc, report) => {
+    if(isSafeWithTolerance(report)) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+
+  return count;
 };
